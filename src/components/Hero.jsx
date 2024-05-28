74e3528b0dbf3b3a6596bdd33365c986e5d0384a
Guocork <g1024536444@gmail.com>
2024-05-28 20:15:47 +0800

    引入gsap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,11 @@
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p className="hero-title">iPhone 15 Pro</p>
+      </div>
+    </section>
   )
 }
 
