4e3c78cb532b288908e15f6565453898927f4480
Guocork <g1024536444@gmail.com>
2024-05-28 19:13:29 +0800

    完成navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
                 ))}
             </div>
 
-            <div>
+            <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
                 <img src={searchImg} alt="search" width={18} height={18}/>
                 <img src={bagImg} alt="bag" width={18} height={18}/>
             </div>
