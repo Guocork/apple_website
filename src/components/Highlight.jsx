0d36caee45a05ce5d3de7575eca8884180f99735
Guocork <g1024536444@gmail.com>
2024-05-28 23:13:02 +0800

    视频列表

--- src/components/Highlight.jsx
+++ src/components/Highlight.jsx
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap";
 import { rightImg, watchImg } from "../utils";
+import VideoCarousel from "./VideoCarousel";
 
 
 
@@ -29,6 +30,8 @@ const Highlight = () => {
           </div>
         </div>
       </div>
+
+      <VideoCarousel />
     </section>
   )
 }
---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,31 @@
+import { hightlightsSlides } from "../constants"
+
+const VideoCarousel = () => {
+    return (
+        <>
+        <div className="flex items-center">
+            {hightlightsSlides.map((list, i) => (
+                <div key={list.id} id="slider" className="sm:pr-20 pr-10">
+                    <div className="video-carousel_container">
+                        <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                            <video id="video" playsInline={true} preload="auto" muted>
+                                <source src={list.video} type="video/mp4"/>
+                            </video>
+                        </div>
+
+                        <div className="absolute top-12 left-[5%] z-10">
+                            {list.textLists.map((text) => (
+                                <p key={text} className="md:text-2xl text-xl font-medium">
+                                    {text}
+                                </p>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+        </>
+    )
+}
+
+export default VideoCarousel
