d3893a970794d136caab6d31a438f1da767b90eb
Guocork <g1024536444@gmail.com>
2024-06-03 22:05:51 +0800

    逻辑

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,47 +1,82 @@
-import { useRef } from "react"
-import { hightlightsSlides } from "../constants"
+import { useRef } from "react";
+import { hightlightsSlides } from "../constants";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const VideoCarousel = () => {
-
     const videoRef = useRef([]);
     const videoSpanRef = useRef([]);
     const videoDivRef = useRef([]);
 
-    const [ video, setVideo ] = useState({
+    const [video, setVideo] = useState({
         isEnd: false,
         startPlay: false,
-        videoId: false,
+        videoId: 0,
         isLastVideo: false,
         isPlaying: false,
     });
 
-    const {isEnd,startPlay,videoId,isLastVideo,isPlaying} = video;
+    const [loadedData, setLoadData] = useState([]);
+
+    const { isEnd, startPlay, videoId, isLastVideo, isPlaying } = video;
+
+    useEffect(() => {
+        if (loadedData.length > 3) {
+            if (!isPlaying) {
+                videoRef.current[videoId].pause();
+            } else {
+                startPlay && videoRef.current[videoId].play();
+            }
+        }
+    }, [startPlay, videoId, isPlaying, loadedData]);
+
+    useEffect(() => {
+        const currentProgress = 0;
+        let span = videoSpanRef.current;
+    }, [videoId, startPlay]);
+
+    if (span[videoId]) {
+        let anim = gsap.to(span[videoId], {
+            onUpdate: () => { },
+            onComplete: () => { },
+        });
+    }
     return (
         <>
-        <div className="flex items-center">
-            {hightlightsSlides.map((list, i) => (
-                <div key={list.id} id="slider" className="sm:pr-20 pr-10">
-                    <div className="video-carousel_container">
-                        <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
-                            <video id="video" playsInline={true} preload="auto" muted>
-                                <source src={list.video} type="video/mp4"/>
-                            </video>
-                        </div>
+            <div className="flex items-center">
+                {hightlightsSlides.map((list, i) => (
+                    <div key={list.id} id="slider" className="sm:pr-20 pr-10">
+                        <div className="video-carousel_container">
+                            <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                                <video
+                                    id="video"
+                                    playsInline={true}
+                                    preload="auto"
+                                    muted
+                                    ref={(el) => (video.current[i] = el)}
+                                    onPlay={() => {
+                                        setVideo((prevVideo) =>({
+                                            ...prevVideo, isPlaying: true
+                                        }))
+                                    }}
+                                >
+                                    <source src={list.video} type="video/mp4" />
+                                </video>
+                            </div>
 
-                        <div className="absolute top-12 left-[5%] z-10">
-                            {list.textLists.map((text) => (
-                                <p key={text} className="md:text-2xl text-xl font-medium">
-                                    {text}
-                                </p>
-                            ))}
+                            <div className="absolute top-12 left-[5%] z-10">
+                                {list.textLists.map((text) => (
+                                    <p key={text} className="md:text-2xl text-xl font-medium">
+                                        {text}
+                                    </p>
+                                ))}
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default VideoCarousel
+export default VideoCarousel;
